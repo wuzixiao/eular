5b065ae9454539adc9054ca3152b2c03d514ad10
LiuMaoyang <wuzi.xiao@gmail.com>
2016-06-07 22:35:00 +1200

    better 10

--- 8_largest_product_in_serial.hs
+++ 8_largest_product_in_serial.hs
@@ -1,14 +1,52 @@
+{-# LANGUAGE FlexibleContexts #-}
 -- Convert string to Int list
 -- sToL :: Integral a => String -> [a]
 -- sToL = map (read . return)
 
-import Data.Char
+import           Data.Char
+import           Data.List
+
+str = "73167176531330624919225119674426574742355349194934\
+\96983520312774506326239578318016984801869478851843\
+\85861560789112949495459501737958331952853208805511\
+\12540698747158523863050715693290963295227443043557\
+\66896648950445244523161731856403098711121722383113\
+\62229893423380308135336276614282806444486645238749\
+\30358907296290491560440772390713810515859307960866\
+\70172427121883998797908792274921901699720888093776\
+\65727333001053367881220235421809751254540594752243\
+\52584907711670556013604839586446706324415722155397\
+\53697817977846174064955149290862569321978468622482\
+\83972241375657056057490261407972968652414535100474\
+\82166370484403199890008895243450658541227588666881\
+\16427171479924442928230863465674813919123162824586\
+\17866458359124566529476545682848912883142607690042\
+\24219022671055626321111109370544217506941658960408\
+\07198403850962455444362981230987879927244284909188\
+\84580156166097919133875499200524063689912560717606\
+\05886116467109405077541002256983155200055935729725\
+\71636269561882670428252483600823257530420752963450"
+
 
 sToL :: [Char] -> [Int]
 -- But this will fail. sToL :: Int a => [Char] -> [a]
---String is a type, and you are using it as a type class. A type class is a collection of types, rather than a single type, e.g. Integer and Double and Rational are all types belonging to the type class Num. Type classes appear to the left of => in types, where real types and type variables appear to the right of =>
+-- Because String is a type, and you are using it as a type class. A type class is a collection of types, rather than a single type, e.g. Integer and Double and Rational are all types belonging to the type class Num. Type classes appear to the left of => in types, where real types and type variables appear to the right of =>
 
 
-sToL str = map digitToInt str
+sToL s = map digitToInt s
+
+maxProduct xss = maximum (map product xss)
+
+makeList xs = foldr (zipWith(:)) (repeat []) (take 13 (tails xs))
+
+answer str = maxProduct ( makeList (sToL str))
+
+
+
+
+
+
+
+
 
 
